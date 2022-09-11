f101a88d45c90f463ac718161aaba427a77276c0
ivanRamiro05 <ivan2224654@correo.uis.edu.co>
2022-09-11 17:00:42 -0500

    actualizacion de mi proyecto

---
+++ README.md
@@ -0,0 +1,2 @@
+# juego_la_21
+objetivos : crear un juego que pregunte la cantidad de jugadores que se repartan las cartas y también se pushee a ganar la partida
---
+++ juego_la_21.py
@@ -0,0 +1,29 @@
+#JUEGO LA 21
+
+print("----------------------------")
+
+print("-------Reglas del juego--------")
+print("1.0 no dañar el programa ")
+print("2.0 las apuestas se realizaran fuera del juego")
+print("3.0 el jugador que se pase el 21 puntos pierde")
+
+print("¿aceptas las reglas del juego?")
+print("si. pon un si en la badeja de respuestas")
+print("no. pon un no en la badeja de respuestas")
+opcion=(input("\n Dame la opción: "))
+if (opcion==si):
+"sigue con el juego"
+else:
+"el juego se acaba aca"
+
+jugadores=int(input("ingrese la cantidad de jugadores de la partida: "))
+
+print("\n ¿cómo quieres jugar?:   \n")
+#se despliega el menu de juego
+
+print("1.La 21 maquina vs un jugador")
+print("2.La 21 maquina vs dos jugadores")
+print("3.La 21 maquina vs tres jugadores")
+print("4.La 21 maquina vs cuatro jugadores")
+
+
