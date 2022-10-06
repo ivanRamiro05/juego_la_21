ec4c5fab707b3fc5f89555567217b55097c373b5
ivanRamiro05 <ivan2224654@correo.uis.edu.co>
2022-10-05 20:24:51 -0500

    Ejercicio nice

--- .py
+++ .py
@@ -4,58 +4,64 @@ import random
 #processing
 jugador=random.randint(1,10)
 pc=random.randint(1,10)
-dan=0
-sum=0
+#Numeros aleatorios tanto del jugador como del pc"Representando las cartas del juego"
+#sum jugador
+#dan pac
+suma_de_puntos_tallador=0
+suma_de_puntos_Jugador=0
 i=1
-diferencia=dan-sum
+diferencia=suma_de_puntos_tallador-suma_de_puntos_Jugador
 while i<= 2:
-    sum=sum+jugador
-    dan=dan+pc
+    suma_de_puntos_Jugador=suma_de_puntos_Jugador+jugador
+    suma_de_puntos_tallador=suma_de_puntos_tallador+pc
     i=i+1
-print("pc",dan)
-print("Jugador",sum)
+print("pc",suma_de_puntos_tallador)
+print("Jugador",suma_de_puntos_Jugador)
 print("¿Quieres seguir jugando?")
 X=int(input("1. para seguir jugando o 2. para parar: "))
 if X==1: 
-    while sum<=21 and X!=2:
-        d= random.randint(1,10)  
-        sum =sum +d 
-        print("jugador",sum)
-        if sum>21:
+    while suma_de_puntos_Jugador<=21 and X!=2:
+        #este whilw lo uso para generar el bucle si el jugador seguir jugando
+        d= random.randint(1,10)
+        #decidí crear una variable nueva "d" para representar una nueva variable aleatoria  
+        suma_de_puntos_Jugador=suma_de_puntos_Jugador +d 
+        print("jugador",suma_de_puntos_Jugador)
+        if suma_de_puntos_Jugador>21:
             print("volaste")
             break
+        #Este break es para parar el programa si el jugador llegase a perder
         print("¿Quieres seguir jugando?")
         X=int(input("1. para seguir jugando o 2. para parar: "))
-        if sum>21:
+        if suma_de_puntos_Jugador>21:
             print("volaste")
             break
         elif X==2:
-            while dan <=sum and dan < 21:
+            while suma_de_puntos_tallador<=suma_de_puntos_Jugador and suma_de_puntos_tallador< 21:
                 p=random.randint(1,10)
-                dan = dan + p
-                print("pc", dan)
-            if dan>sum and dan<21:
-                if dan-sum==1:
+                suma_de_puntos_tallador = suma_de_puntos_tallador + p
+                print("Tallador", suma_de_puntos_tallador)
+            if suma_de_puntos_tallador>suma_de_puntos_Jugador and suma_de_puntos_tallador<21:
+                if suma_de_puntos_tallador-suma_de_puntos_Jugador==1:
                     print("El tallador gana por un punto")
                 else:
-                    print("gana el tallador por" , dan-sum, "Puntos")
-            elif dan==sum:
+                    print("gana el tallador por" , suma_de_puntos_tallador-suma_de_puntos_Jugador, "Puntos")
+            elif suma_de_puntos_tallador==suma_de_puntos_Jugador:
                 print("gana el pc por ser el tallador")
-            if dan>21:
+            if suma_de_puntos_tallador>21:
                 print("El tallador ah volado")
 
 elif X==2:
-    while dan <=sum and dan < 21:
-        p=random.randint(1,10)
-        dan = dan + p
-        print("pc", dan)
-    if dan>sum and dan<21:
-        if dan -sum==1:
-            print("El tallador gana por un punto")
-        else:
-            print("gana el tallador por" , dan-sum, "Puntos")
-    elif dan==sum:
-        print("gana el pc por ser el tallador")
-    if dan>21:
-        print("El tallador ah volado")
+            while suma_de_puntos_tallador<=suma_de_puntos_Jugador and suma_de_puntos_tallador< 21:
+                p=random.randint(1,10)
+                suma_de_puntos_tallador = suma_de_puntos_tallador + p
+                print("Tallador", suma_de_puntos_tallador)
+            if suma_de_puntos_tallador>suma_de_puntos_Jugador and suma_de_puntos_tallador<21:
+                if suma_de_puntos_tallador-suma_de_puntos_Jugador==1:
+                    print("El tallador gana por un punto")
+                else:
+                    print("gana el tallador por" , suma_de_puntos_tallador-suma_de_puntos_Jugador, "Puntos")
+            elif suma_de_puntos_tallador==suma_de_puntos_Jugador:
+                print("gana el pc por ser el tallador")
+            if suma_de_puntos_tallador>21:
+                print("El tallador ah volado")
         
